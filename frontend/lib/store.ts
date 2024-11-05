072440b3fda8698bdd1329050b23334d27ff2888
Somya Ranjan Khatua <115935107+somyaranjan007@users.noreply.github.com>
2024-11-05 16:29:39 +0530

    updated user clice

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import useReducer from "@/lib/reducers/user_data_slice";
 export const makeStore = () => {
     return configureStore({
         reducer: {
-
+            user: useReducer
         },
         middleware: (getDefaultMiddleware) => getDefaultMiddleware({
             serializableCheck: false,
